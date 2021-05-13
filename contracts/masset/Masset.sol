378a6ea080f981aab676756e0d31365e3c8bcbe7
Dhark Mattr <dharkmattr@gmail.com>
2021-05-13 23:20:49 +0300

    IERC1820Registry in intializer

--- contracts/masset/Masset.sol
+++ contracts/masset/Masset.sol
@@ -32,7 +32,7 @@ contract Masset is
     );
 
     // state
-    //IERC1820Registry constant private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    IERC1820Registry constant private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
     address private basketManager;
 
     // public
@@ -42,7 +42,7 @@ contract Masset is
 
         InitializableToken._initialize(_name, _symbol);
         InitializableReentrancyGuard._initialize();
-        //_erc1820.setInterfaceImplementer(address(this), keccak256("ERC777TokensRecipient"), address(this));
+        _erc1820.setInterfaceImplementer(address(this), keccak256("ERC777TokensRecipient"), address(this));
 
         basketManager = _basketManager;
     }
