40bbea4f32ec599ef1bb2eba363dede14716cb46
Derek Matter <dharkmattr@gmail.com>
2021-06-08 19:40:28 +0300

    after upgrade and migration v1 to v2

--- scripts/src/upgradeMasset.ts
+++ scripts/src/upgradeMasset.ts
@@ -3,9 +3,11 @@ import Truffle from 'truffle';
 
 const massetProxyAddress = '0x4BF113905d7F69202106F613308bb02C84AaDF2F';
 
-export default async function mint(truffle): Promise<any> {
+export default async function mint(truffle, ...args): Promise<any> {
     const wallet: HDWalletProvider = truffle.provider;
     const artifacts: Truffle.Artifacts = truffle.artifacts;
+
+    console.log('deployer: ', truffle.web3.currentProvider.addresses[0]);
 
     const Masset = artifacts.require("Masset");
     const MassetProxy = artifacts.require("MassetProxy");
@@ -17,38 +19,19 @@ export default async function mint(truffle): Promise<any> {
 
     console.log('version: ', await fake.getVersion());
     console.log('token: ', await fake.getToken());
-    console.log('basket manager: ', await fake.geBasketManager());
+    console.log('basket manager: ', await fake.getBasketManager());
 
     console.log(2);
 
-    const masset = await Masset.new();
-    console.log("new masset: ", masset.address);
+    //const masset = await Masset.new();
+    //console.log("new masset: ", masset.address);
 
-    let abi = massetProxy.contract.methods['upgradeTo(address)'](masset.address).encodeABI();
-    console.log('abi for upgrade: ', abi);
+    //let abi = massetProxy.contract.methods['upgradeTo(address)'](masset.address).encodeABI();
+    //console.log('abi for upgrade: ', abi);
 
-    /*
-    const masset = await Masset.new();
+    await fake.migrateFromV1ToV2();
 
-
-    //console.log('current version: ', await fake.getVersion());
-
-    console.log(2);
-
-    const thresholdProxyAdmin = await ThresholdProxyAdmin.at(thresholdProxyAdminAddress);
-
-    console.log(3);
-
-    console.log('current version: ', await fake.getVersion());
-
-    console.log(await fake.getBasketManager());
-    console.log(await fake.getToken());
-
-    await fake.resetReentrancyGuard();
-
-    //await fake.migrateFromV1ToV2();
-
-    //console.log(await fake.geBasketManager());
-    //console.log(await fake.getToken());
-     */
+    console.log('version: ', await fake.getVersion());
+    console.log('token: ', await fake.getToken());
+    console.log('basket manager: ', await fake.getBasketManager());
 }
