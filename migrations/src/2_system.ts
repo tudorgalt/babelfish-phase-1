35ffe71ec5736efc2d4702dfaced413ddef8f0f9
Derek Matter <dharkmattr@gmail.com>
2021-09-12 17:57:58 +0300

    temp

--- migrations/src/2_system.ts
+++ migrations/src/2_system.ts
@@ -81,7 +81,7 @@ export default async ({ artifacts }: { artifacts: Truffle.Artifacts },
     await state.conditionalInitialize('MassetProxy', () => {
         return d_MassetProxy.methods["initialize(address,address,bytes)"](
             d_Masset.address,
-            addresses.multisig,
+            network == 'rsk' ? addresses.multisig : admin,
             initData2,
         );
     });
--- scripts/src/upgradeMasset.ts
+++ scripts/src/upgradeMasset.ts
@@ -1,26 +1,31 @@
 import HDWalletProvider from '@truffle/hdwallet-provider';
 import Truffle from 'truffle';
+import state from '../../migrations/state'
 
-const massetProxyAddress = '0x1440d19436bEeaF8517896bffB957a88EC95a00F';
+const massetProxyAddress = '0x1572D7E4a78A8AD14AE722E6fE5f5600a2c7A149';
 
 export default async function mint(truffle): Promise<any> {
     const wallet: HDWalletProvider = truffle.provider;
     const artifacts: Truffle.Artifacts = truffle.artifacts;
 
+    state.setNetwork('rskTestnet');
+
     const Masset = artifacts.require("Masset");
     const MassetProxy = artifacts.require("MassetProxy");
 
-    const massetProxy = await MassetProxy.at(massetProxyAddress);
+    //const masset = await state.conditionalDeploy(Masset, 'Masset', () => Masset.new());
+
+    //const massetProxy = await MassetProxy.at(massetProxyAddress);
+    //await massetProxy.upgradeTo(masset.address);
+    /*
     const abi = massetProxy.contract.methods['upgradeTo(address)']('0xad624f56f80cDb5E1b37d314981672e24F0917EA').encodeABI();
-    console.log(abi);
+    console.log(abi);\
+    */
 
     const fake = await Masset.at(massetProxyAddress);
-
     console.log(await fake.getVersion());
-
-    //const masset = await Masset.new();
-    //console.log(masset.address);
-
     await fake.migrateV1ToV2();
+    console.log(await fake.getVersion());
+    await fake.migrateV20ToV21();
     console.log(await fake.getVersion());
 }
