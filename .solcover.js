8c4c4d57cc635dfc5f02fac52083dec612e71af7
Michal Topa <michal.topa@rumblefish.pl>
2021-09-20 16:44:33 +0200

    skip mocks and fish contracts in coverage

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,8 @@ module.exports = {
   skipFiles: [
     'Migrations.sol',
     'interfaces',
+    'fish',
+    'mocks',
     'integrations',
     'z_mocks',
     'shared/InitializableReentrancyGuard.sol',
