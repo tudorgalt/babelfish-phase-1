4a408e65b4530626b78dfef2d2ed686f0e384cc0
Derek Matter <dharkmattr@gmail.com>
2021-11-02 22:59:09 +0200

    changed the requirements on mint and burn

--- contracts/mynt/MyntToken.sol
+++ contracts/mynt/MyntToken.sol
@@ -13,22 +13,11 @@ import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract MyntToken is ERC20, ERC20Detailed, Ownable {
 
-    address public controller = address(0);
+    event MarketMakerChanged(address indexed _address);
+    event PresaleChanged(address indexed _address);
 
-    /**
-      * @dev Throws if called by any account other than the controller.
-      */
-    modifier onlyController() {
-        require(isController(), "caller is not the controller");
-        _;
-    }
-
-    /**
-     * @dev Returns true if the caller is the current controller.
-     */
-    function isController() public view returns (bool) {
-        return _msgSender() == controller;
-    }
+    address public marketMaker = address(0);
+    address public presale = address(0);
 
     /**
      * @notice Constructor called on deployment, initiates the contract.
@@ -37,10 +26,22 @@ contract MyntToken is ERC20, ERC20Detailed, Ownable {
 
     /**
      * @notice setConttroller sets the token's conttroller
-     * @param _controller The address of the controller contract
+     * @param _address The address of the controller contract
      * */
-    function setController(address  _controller) public onlyOwner {
-        controller = _controller;
+    function setMarketMaker(address _address) public onlyOwner {
+        require(_address != address(0), "invalid address");
+        marketMaker = _address;
+        emit MarketMakerChanged(_address);
+    }
+
+    /**
+     * @notice setConttroller sets the token's conttroller
+     * @param _address The address of the controller contract
+     * */
+    function setPresale(address _address) public onlyOwner {
+        require(_address != address(0), "invalid address");
+        presale = _address;
+        emit PresaleChanged(_address);
     }
 
     /**
@@ -48,7 +49,9 @@ contract MyntToken is ERC20, ERC20Detailed, Ownable {
      * @param _account The recipient address to get the minted tokens.
      * @param _amount The amount of tokens to be minted.
      * */
-    function mint(address _account, uint256 _amount) public onlyController {
+    function mint(address _account, uint256 _amount) public {
+        // only the presale and the marketmaker are allowed to mint
+        require(msg.sender == presale || msg.sender == marketMaker, "not allowed");
         _mint(_account, _amount);
     }
 
@@ -57,7 +60,10 @@ contract MyntToken is ERC20, ERC20Detailed, Ownable {
      * @param _account The recipient address to get the minted tokens.
      * @param _amount The amount of tokens to be minted.
      * */
-    function burn(address _account, uint256 _amount) public onlyController {
+    function burn(address _account, uint256 _amount) public {
+        // only the marketmake is alloed to burn tokens,
+        // ...and the user is allowed to burn his own tokens
+        require(msg.sender == marketMaker || msg.sender == _account, "not allowed");
         _burn(_account, _amount);
     }
 }
