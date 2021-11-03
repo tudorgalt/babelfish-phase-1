7e67c4ff5c7c6698cd54e072c99ad0777419ed55
Derek Matter <dharkmattr@gmail.com>
2021-11-03 20:57:17 +0200

    updated mynt token deployment script

--- scripts/src/deployMyntToken.ts
+++ scripts/src/deployMyntToken.ts
@@ -9,7 +9,29 @@ export default async function deployMyntToken(truffle, networkName: string): Pro
     const wallet: HDWalletProvider = truffle.provider;
     const artifacts: Truffle.Artifacts = truffle.artifacts;
 
+    console.log('Deploying MYNT token...');
+
     const MyntToken = artifacts.require("MyntToken");
     const myntToken = await conditionalDeploy(MyntToken, `MyntToken`,
         () => MyntToken.new());
+
+    console.log('Deployed at', myntToken.address);
+
+    const amm = '';
+    const presale = '';
+    const multisig = '';
+
+    console.log('Setting market maker address to', amm);
+
+    await myntToken.setMarketMaker(amm);
+
+    console.log('Setting presale address to', presale);
+
+    await myntToken.setPresale(presale);
+
+    console.log('Setting owner address to', multisig);
+
+    await myntToken.transferOwnership(multisig);
+
+    console.log('Done.');
 }
