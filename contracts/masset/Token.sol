53e1f76852a9869f2259cf22fd3d21d0df8bdeb5
Alexandre Wolff <alexandrewlff@gmail.com>
2021-12-04 18:35:38 +0100

    Implement paymaster timelocked update & allowance bypass for paymaster in token contract

--- contracts/masset/Token.sol
+++ contracts/masset/Token.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "./BaseRelayRecipient.sol";
 import "./IApproveAndCall.sol";
 
@@ -14,6 +15,21 @@ import "./IApproveAndCall.sol";
  */
 
 contract Token is ERC20, ERC20Detailed, Ownable, BaseRelayRecipient {
+    using Address for address;
+
+    struct PaymasterUpdate {
+        address newPaymaster;
+        uint256 endGracePeriod;
+        bool hasToBeExecuted;
+    }
+
+    address public paymaster;
+    PaymasterUpdate public paymasterUpdate;
+    mapping(address => bool) public paymasterRevoked;
+
+    event PaymasterUpdateLaunched(address indexed newBridge, uint256 endGracePeriod);
+    event PaymasterUpdateExecuted(address indexed newBridge);
+    event PaymasterRevoked(address account, bool revoked);
 
     /**
      * @notice Constructor called on deployment, initiates the contract.
@@ -21,11 +37,40 @@ contract Token is ERC20, ERC20Detailed, Ownable, BaseRelayRecipient {
      * @param _symbol The symbol of the token.
      * @param _decimals The decimals of the token.
      * */
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) public
-    ERC20Detailed(_name, _symbol, _decimals) {}
-
-    function setTrustedForwarder(address _forwarder) external onlyOwner {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        address _forwarder,
+        address _paymaster
+    ) public
+    ERC20Detailed(_name, _symbol, _decimals) {
         _trustedForwarder = _forwarder;
+     	paymaster = _paymaster;
+    }
+
+    function launchPaymasterUpdate(address newPaymaster) external onlyOwner {
+        require(!paymasterUpdate.hasToBeExecuted, "current update has to be executed");
+        require(newPaymaster.isContract(), "address provided is not a contract");
+
+        uint256 endGracePeriod = block.timestamp + 1 weeks;
+
+        paymasterUpdate = PaymasterUpdate(newPaymaster, endGracePeriod, true);
+
+        emit PaymasterUpdateLaunched(newPaymaster, endGracePeriod);
+    }
+
+    function executePaymasterUpdate() external onlyOwner {
+        require(
+            paymasterUpdate.endGracePeriod <= block.timestamp,
+            "grace period has not finished"
+        );
+        require(paymasterUpdate.hasToBeExecuted, ": update already executed");
+
+        paymasterUpdate.hasToBeExecuted = false;
+        paymaster = paymasterUpdate.newPaymaster;
+
+        emit PaymasterUpdateExecuted(paymasterUpdate.newPaymaster);
     }
 
     /**
@@ -63,6 +108,20 @@ contract Token is ERC20, ERC20Detailed, Ownable, BaseRelayRecipient {
 		approve(_spender, _amount);
 		IApproveAndCall(_spender).receiveApproval(msg.sender, _amount, address(this), _data);
 	}
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        if (msg.sender == paymaster && !paymasterRevoked[sender]) {
+            _transfer(sender, recipient, amount);
+        } else {
+            super.transferFrom(sender, recipient, amount);
+        }
+        return true;
+    }
+
+    function revokePaymaster(bool revoke) external {
+        paymasterRevoked[_msgSender()] = revoke;
+        emit PaymasterRevoked(_msgSender(), revoke);
+    }
 
     function _msgSender() internal view returns (address payable ret) {
         return BaseRelayRecipient._msgSender();
