a2c6109e58a7626701111c4d6d7088343f87a1fa
meho <michal.t1506@gmail.com>
2022-03-21 09:55:42 +0100

    do not send rbtc

--- scripts/tasks/getTokens.ts
+++ scripts/tasks/getTokens.ts
@@ -9,13 +9,13 @@ const logger = new Logs().showInConsole(true);
 const RECIPIENT = "0x5170F3D5158653DDd8649c483cd1cd9bae6803EF";
 
 const TOKEN_AMOUNT = tokens("10000");
-const RBTC_AMOUNT = tokens("10");
+// const RBTC_AMOUNT = tokens("10");
 
 // const TOKEN_SYMBOL = "FishToken";
 const TOKEN_ADDRESS = "0x5250D37B096099678b0957bae32153915ca2C043";
 
 task("getTokens", "mints tokens and transfers RBTC").setAction(async (_, hre) => {
-    const { network, web3, artifacts } = hre;
+    const { network, artifacts } = hre;
     setNetwork(network.name);
     const [owner] = await hre.getUnnamedAccounts();
 
@@ -26,6 +26,6 @@ task("getTokens", "mints tokens and transfers RBTC").setAction(async (_, hre) =>
     await token.mint(RECIPIENT, TOKEN_AMOUNT, { from: owner });
     logger.success(`Minted ${fromWei(TOKEN_AMOUNT)} tokens for ${RECIPIENT}`);
 
-    await web3.eth.sendTransaction({ from: owner, to: RECIPIENT, value: RBTC_AMOUNT });
-    logger.success(`Transferred ${fromWei(RBTC_AMOUNT)} RBTC for ${RECIPIENT}`);
+    // await web3.eth.sendTransaction({ from: owner, to: RECIPIENT, value: RBTC_AMOUNT });
+    // logger.success(`Transferred ${fromWei(RBTC_AMOUNT)} RBTC for ${RECIPIENT}`);
 });
